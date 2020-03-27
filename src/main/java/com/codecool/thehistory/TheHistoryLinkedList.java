248dfc953227ce5dc543cada25cd8cf89ab8d2ba
dandumitriu33 <dandumitriu33@gmail.com>
2020-03-27 15:04:08 +0200

    Refactor LinkedList - performance successful

--- src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
@@ -34,62 +34,125 @@ public class TheHistoryLinkedList implements TheHistory {
 
     @Override
     public void replaceOneWord(String from, String to) {
-        while (wordsLinkedList.contains(from)) {
-            int tempIndex = wordsLinkedList.indexOf(from);
-            wordsLinkedList.set(tempIndex, to);
+        ListIterator<String> iter = wordsLinkedList.listIterator();
+        while(iter.hasNext()) {
+            String tempWord=iter.next();
+            if (tempWord.equals(from)) {
+                iter.previous();
+                iter.remove();
+                iter.add(to);
+            }
         }
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         System.out.println(wordsLinkedList);
-        LinkedList<String> resultList = new LinkedList<String>();
+//        LinkedList<String> resultList = new LinkedList<String>();
         String[] temp = new String[fromWords.length];
-        System.out.println("res list: "+resultList);
+//        System.out.println("res list: "+resultList);
         if (wordsLinkedList.containsAll(Arrays.asList(fromWords))) {
-            System.out.println("ini from: "+Arrays.asList(fromWords));
-            System.out.println("ini to: "+Arrays.asList(toWords));
-            int i=0;
-            while (i<wordsLinkedList.size()) {
-                System.out.println("i: "+i+" < " + wordsLinkedList.size());
-            if (!wordsLinkedList.get(i).equals(fromWords[0])) {
-                System.out.println("i: "+i + " wordsLL[i]: " + wordsLinkedList.get(i));
-                resultList.add(wordsLinkedList.get(i));
-                i++;
-            }
-            else {
-                try {
-                ListIterator list_Iter = wordsLinkedList.listIterator(i);
+            ListIterator<String> iter = wordsLinkedList.listIterator();
 
-                    for (int j = 0; j < fromWords.length; j++) {
-                        temp[j] = (String) list_Iter.next();
+            while (iter.hasNext()) {
+                String tempWord = iter.next();
+                System.out.println("mv forward");
+                System.out.println("looking at: " + tempWord);
+                if (tempWord.equals(fromWords[0])) {
+                    iter.previous();
+                    System.out.println("mv back");
+                    int counter = 0;
+                    for (int i=0; i<fromWords.length; i++) {
+                        if (iter.hasNext()) {
+                            temp[i] = iter.next();
+                            counter++;
+                            System.out.println("mv fw");
+                        }
                     }
-                    System.out.println("temp: " + Arrays.toString(temp) + " fromWords: " + Arrays.toString(fromWords));
+                    if (counter!=fromWords.length) {
+                        for (int i=0; i<counter; i++) {
+                            iter.previous();
+                            System.out.println("mv bk repair");
+                        }
+                        counter=0;
+                        temp=null;
+                    }
+                    else {
+                        for (int j = 0; j < fromWords.length; j++) {
+                            if (iter.hasPrevious()) {
+                                iter.previous();
+                                System.out.println("mv bk");
+                            }
+                        }
+                    }
                     if (Arrays.equals(temp, fromWords)) {
-                        System.out.println("eq\n");
-                        resultList.addAll(Arrays.asList(toWords));
-                        System.out.println("workd");
-                        i += fromWords.length;
-                        System.out.println("i strange?: " + i);
-                    } else {
-                        System.out.println("no eq\n");
-                        resultList.add(wordsLinkedList.get(i));
-                        i++;
+                        System.out.println("eq");
+                        for (int i=0; i<fromWords.length; i++) {
+                            iter.next();
+                            iter.remove();
+                            System.out.println("rm fw");
+                        }
+                        for (int j=0; j<toWords.length; j++) {
+//                            iter.next();
+                            iter.add(toWords[j]);
+                            System.out.println("add fw");
+                        }
+                    }
+                    else {
+                        iter.next();
                     }
                 }
-                catch (NoSuchElementException e) {
-                    resultList.add(wordsLinkedList.get(i));
-                    i++;
+                else {
+                    continue;
                 }
             }
-            }
 
-            wordsLinkedList.clear();
-            ListIterator list_Iter2 = resultList.listIterator(0);
-            while (list_Iter2.hasNext()) {
-                wordsLinkedList.add((String) list_Iter2.next());
-            }
+
+
         }
+//            System.out.println("ini from: "+Arrays.asList(fromWords));
+//            System.out.println("ini to: "+Arrays.asList(toWords));
+//            int i=0;
+//            while (i<wordsLinkedList.size()) {
+//                System.out.println("i: "+i+" < " + wordsLinkedList.size());
+//            if (!wordsLinkedList.get(i).equals(fromWords[0])) {
+//                System.out.println("i: "+i + " wordsLL[i]: " + wordsLinkedList.get(i));
+//                resultList.add(wordsLinkedList.get(i));
+//                i++;
+//            }
+//            else {
+//                try {
+//                ListIterator list_Iter = wordsLinkedList.listIterator(i);
+//
+//                    for (int j = 0; j < fromWords.length; j++) {
+//                        temp[j] = (String) list_Iter.next();
+//                    }
+//                    System.out.println("temp: " + Arrays.toString(temp) + " fromWords: " + Arrays.toString(fromWords));
+//                    if (Arrays.equals(temp, fromWords)) {
+//                        System.out.println("eq\n");
+//                        resultList.addAll(Arrays.asList(toWords));
+//                        System.out.println("workd");
+//                        i += fromWords.length;
+//                        System.out.println("i strange?: " + i);
+//                    } else {
+//                        System.out.println("no eq\n");
+//                        resultList.add(wordsLinkedList.get(i));
+//                        i++;
+//                    }
+//                }
+//                catch (NoSuchElementException e) {
+//                    resultList.add(wordsLinkedList.get(i));
+//                    i++;
+//                }
+//            }
+//            }
+//
+//            wordsLinkedList.clear();
+//            ListIterator list_Iter2 = resultList.listIterator(0);
+//            while (list_Iter2.hasNext()) {
+//                wordsLinkedList.add((String) list_Iter2.next());
+//            }
+//        }
 
 
 //            System.out.println("ini from: "+Arrays.asList(fromWords));
